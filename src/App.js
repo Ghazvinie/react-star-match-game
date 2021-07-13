80cf90e2004a84473466662b4fbff956df08e694
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-13 18:05:49 +0100

    initial upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+// import React from 'react';
+
+// // const Main = ({title}) => (
+// //   <div className="header">{title}</div>
+// // );
+
+// class Main extends React.Component {
+
+//   render () {
+//     return   <div className="header">{this.props.title}</div>
+
+//   }
+// }
+
+
+
+
+// const App = ({title}) => (
+//   <Main title="The GitHub Cards App" />
+// );
+
+
+export default App;
